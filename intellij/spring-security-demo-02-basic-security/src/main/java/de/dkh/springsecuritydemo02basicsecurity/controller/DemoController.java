5ff122fc68671e2ea9447035d23bc81d2938e9d6
dkh <dkh@cp-bap.de>
2023-03-13 13:36:39 +0100

    Spring security: basisc example with in memory auth

---
+++ intellij/spring-security-demo-02-basic-security/src/main/java/de/dkh/springsecuritydemo02basicsecurity/controller/DemoController.java
@@ -0,0 +1,18 @@
+package de.dkh.springsecuritydemo02basicsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    /**
+     * Mapping for {@code home.jsp} for the homepage.
+     *
+     * @return
+     */
+    @GetMapping("/")
+    public String showHome() {
+        return "home";
+    }
+}
---
+++ intellij/spring-security-demo-02-basic-security/target/spring-security-demo/WEB-INF/view/home.jsp
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Homepage</title>
+</head>
+<body>
+
+<h2>Company home page</h2>
+<hr>
+
+Welcome to company home page
+
+</body>
+</html>
