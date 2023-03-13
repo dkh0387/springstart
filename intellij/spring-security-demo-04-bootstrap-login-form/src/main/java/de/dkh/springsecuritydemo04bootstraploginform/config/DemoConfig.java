dc850147c8c91ab87db03c3480c0e90b91c6ef18
dkh <dkh@cp-bap.de>
2023-03-13 16:32:02 +0100

    Spring security: example using custom login form designed with bootstrap

---
+++ intellij/spring-security-demo-04-bootstrap-login-form/src/main/java/de/dkh/springsecuritydemo04bootstraploginform/config/DemoConfig.java
@@ -0,0 +1,35 @@
+package de.dkh.springsecuritydemo04bootstraploginform.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Spring project configuration WITHOUT xml. Everything here is the replacement for {@code dispatcher-servlet.xml}.
+ * {@linkplain EnableWebMvc} replaces {@code <mvc:annotation-driven/>}
+ * (support for conversion, formatting and validation).
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan("de.dkh.springsecuritydemo04bootstraploginform")
+public class DemoConfig {
+
+    /**
+     * Define a bean for ViewResolver to find .jsp pages like:
+     * <bean
+     * class="org.springframework.web.servlet.view.InternalResourceViewResolver">
+     * <property name="prefix" value="/WEB-INF/classes/templates/"/>
+     * <property name="suffix" value=".jsp"/>
+     * </bean>
+     */
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/view/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
