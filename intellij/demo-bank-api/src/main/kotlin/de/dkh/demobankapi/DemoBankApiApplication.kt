864918e238d115acbd6e004d16c9f18df5a1eb3b
dkh0387 <deniskh87@gmail.com>
2023-04-21 09:50:41 +0200

    spring_kotlin: @JsonProperty added for data class to match the JSON key names. Usage of RestTemplate in order to consume data from another RESTAPI

--- intellij/demo-bank-api/src/main/kotlin/de/dkh/demobankapi/DemoBankApiApplication.kt
+++ intellij/demo-bank-api/src/main/kotlin/de/dkh/demobankapi/DemoBankApiApplication.kt
@@ -2,9 +2,19 @@ package de.dkh.demobankapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.web.client.RestTemplate
 
 @SpringBootApplication
-class DemoBankApiApplication
+class DemoBankApiApplication {
+    /**
+     * We need to provide a bean for {@code RestTemplate},
+     * otherwise it can not been autowired in {@code ExternalAPIRepository}.
+     */
+    @Bean
+    fun createRestTemplateBean(builder: RestTemplateBuilder): RestTemplate = builder.build()
+}
 
 fun main(args: Array<String>) {
     runApplication<DemoBankApiApplication>(*args)
