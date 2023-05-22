2e0207c31bf9366194df9a7c94aa9776fd57fc11
dkh0387 <deniskh87@gmail.com>
2023-05-22 18:20:59 +0200

    producer: command line runner added to the main for printing event data

--- intellij/kafka-wikimedia/kafka-wikimedia-producer/src/main/kotlin/de/dkh/kafkawikimediaproducer/KafkaWikimediaProducerApplication.kt
+++ intellij/kafka-wikimedia/kafka-wikimedia-producer/src/main/kotlin/de/dkh/kafkawikimediaproducer/KafkaWikimediaProducerApplication.kt
@@ -1,10 +1,20 @@
 package de.dkh.kafkawikimediaproducer
 
+import com.launchdarkly.eventsource.EventHandler
+import de.dkh.kafkawikimediaproducer.kafka.WikimediaChangesHandler
+import de.dkh.kafkawikimediaproducer.kafka.WikimediaChangesProducer
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class KafkaWikimediaProducerApplication
+class KafkaWikimediaProducerApplication(private val wikimediaChangesProducer: WikimediaChangesProducer) :
+    CommandLineRunner {
+    override fun run(vararg args: String?) {
+        wikimediaChangesProducer.sendMessage()
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<KafkaWikimediaProducerApplication>(*args)
